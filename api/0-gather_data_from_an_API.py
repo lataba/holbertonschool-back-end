d908c075029ad51963ed6fa8e13de2636d223d5e
lataba <ltballeste@gmail.com>
2023-08-27 19:30:53 -0700

    Fix outputs

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,18 +6,23 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    api_url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(
-        api_url + "users/{}".format(argv[1])).json()
-    todos = requests.get(
-        api_url + "todos",
-        params={"userId": argv[1]}).json()
+    employee_id = argv[1]
+    completed_task = 0
+    total_tasks = 0
+    completed_task_titles = []
 
-    completed = []
+    api_url = "https://jsonplaceholder.typicode.com/"
+
+    user = requests.get(api_url + f"users/{employee_id}").json()
+    todos = requests.get(api_url + "todos", params={"userId": employee_id}).json()
+
     for task in todos:
-        if task.get("completed") is True:
-            completed.append(task.get("title"))
+        if task.get("completed"):
+            completed_task += 1
+            completed_task_titles.append(task.get("title"))
+        total_tasks += 1
+
     print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    for complete in completed:
-        print("\t {}".format(complete))
+        user.get("name"), completed_task, total_tasks))
+    for task_title in completed_task_titles:
+        print("\t {}".format(task_title))
